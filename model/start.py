f710c8afe91720e805506793f831885cd4b55db1
beiyanpiki <sizegaoxin@outlook.com>
2023-10-10 13:23:25 +0800

    refactor: change start_attr

--- model/start.py
+++ model/start.py
@@ -2,15 +2,15 @@ from .constants import FrameType
 
 
 class StartAttr:
-    frame_attr: FrameType
+    frame: FrameType
 
-    def __init__(self, frame_attr: FrameType) -> None:
-        self.frame_attr = frame_attr
+    def __init__(self, frame: FrameType) -> None:
+        self.frame = frame
 
     def __json__(self):
         return {
-            "frame_attr": self.frame_attr.value,
+            "frame": self.frame.value,
         }
 
     def __text__(self) -> str:
-        return ""
+        return f"{self.frame.value}"
